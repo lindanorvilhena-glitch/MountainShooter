1da4eadb2d1af74d093dee428bc11ca50b4dfe94
LindanorVC <lindanorvilhena@gmail.com>
2025-08-22 22:32:54 -0300

    Inicial commit

---
+++ code/Const.py
@@ -0,0 +1,22 @@
+from turtledemo.nim import COLOR
+
+from pygame.examples.grid import WINDOW_WIDTH, WINDOW_HEIGHT
+
+#C
+COLOR_ORANGE = (255, 125, 0)
+COLOR_YELLOW = (255, 255, 128)
+COLOR_WHITE = (255, 255, 255)
+COLOR_GREEN = (0, 128, 0)
+COLOR_CYAN = (0, 128, 128)
+
+
+# M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+#W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
